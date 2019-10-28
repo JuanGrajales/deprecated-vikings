b5ef4aeabe7e4a470748559eba0f909a24d9ec60
Juan Grajales <juanfe.gl11@gmail.com>
2019-10-28 10:44:32 -0400

    add attack and recieveDamage method

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -16,33 +16,12 @@ class Soldier {
 
 // Viking
 class Viking extends Soldier {
-  constructor(name, health, strength) {
-    super(health, strength);
-    this.name = name;
-  }
 
-  receiveDamage(theDamage) {
-    super.receiveDamage(theDamage);
-    if(this.health > 0)
-    return `${this.name} has received ${theDamage} points of damage`;
-    else
-    return `${this.name} has died in act of combat`;
-  }
-
-  battleCry() {
-    return `Odin Owns You All!`;
-  }
 }
 
 // Saxon
 class Saxon extends Soldier {
-  receiveDamage(theDamage) {
-    super.receiveDamage(theDamage);
-    if(this.health > 0)
-    return `A Saxon has received ${theDamage} points of damage`;
-    else
-    return `A Saxon has died in combat`;
-  }
+
 }
 
 // War
